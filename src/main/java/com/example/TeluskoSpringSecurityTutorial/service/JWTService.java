3184ee7a3aaf7738ee8bcc214ead665b4eeef4f1
AllasKevin <kevin_ruonala@hotmail.com>
2025-04-01 19:37:45 +0200

    Finished generating JWT Token section.

--- src/main/java/com/example/TeluskoSpringSecurityTutorial/service/JWTService.java
+++ src/main/java/com/example/TeluskoSpringSecurityTutorial/service/JWTService.java
@@ -1,10 +1,50 @@
 package com.example.TeluskoSpringSecurityTutorial.service;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class JWTService {
-    public String genereateToken() {
-        return "JWT Token";
+
+    private String secretKey = "";
+    // TODO: An option is to create a key everytime this service is used and not just when it is created like below.
+    public JWTService() {
+        try {
+            KeyGenerator keyGen = KeyGenerator.getInstance("HmacSHA256");
+            SecretKey sk = keyGen.generateKey();
+            secretKey = Base64.getEncoder().encodeToString(sk.getEncoded());
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String genereateToken(String username) {
+        Map<String, Object> claims = new HashMap<>();
+
+        return Jwts.builder()
+                .claims()
+                .add(claims)
+                .subject(username)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 30)) // 30 minutes
+                .and()
+                .signWith(getKey())
+                .compact();
+    }
+
+    private Key getKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
