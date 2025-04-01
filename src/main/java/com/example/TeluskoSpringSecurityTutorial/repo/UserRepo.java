fcecbf77fd14bd34cc3328d21d29fe5bd3900d69
AllasKevin <kevin_ruonala@hotmail.com>
2025-04-01 15:51:38 +0200

    Finished User from database section.

---
+++ src/main/java/com/example/TeluskoSpringSecurityTutorial/repo/UserRepo.java
@@ -0,0 +1,8 @@
+package com.example.TeluskoSpringSecurityTutorial.repo;
+
+import com.example.TeluskoSpringSecurityTutorial.model.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<Users, Integer> {
+    Users findByUsername(String username);
+}
