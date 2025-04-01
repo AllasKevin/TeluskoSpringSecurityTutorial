de9adb6a3b31e71e521606de3441762b21ec549d
AllasKevin <kevin_ruonala@hotmail.com>
2025-04-01 10:53:53 +0200

    intialized project with spring security and one controller.

---
+++ src/main/java/com/example/TeluskoSpringSecurityTutorial/TeluskoSpringSecurityTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.TeluskoSpringSecurityTutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeluskoSpringSecurityTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeluskoSpringSecurityTutorialApplication.class, args);
+	}
+
+}
