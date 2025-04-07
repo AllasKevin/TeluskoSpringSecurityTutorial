7cbea2e30f60ab7dc97d36b30f9c6ad2473108bd
AllasKevin <kevin_ruonala@hotmail.com>
2025-04-07 19:24:37 +0200

    Added Static files from frontend-poc. Changed sameSite policy to None from strict on the returning cookie and added more allowed origins.

--- src/main/java/com/example/TeluskoSpringSecurityTutorial/config/WebMvcConfig.java
+++ src/main/java/com/example/TeluskoSpringSecurityTutorial/config/WebMvcConfig.java
@@ -1,9 +1,17 @@
 package com.example.TeluskoSpringSecurityTutorial.config;
 
+import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+import org.springframework.web.servlet.resource.ResourceResolverChain;
+
+import java.util.List;
+
+import static java.util.Objects.nonNull;
 /*
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
@@ -19,5 +27,28 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**")
                 .addResourceLocations("classpath:/static/");
+    }
+
+    private void serveDirectory(ResourceHandlerRegistry registry, String endpoint, String location) {
+        // 1
+        String[] endpointPatterns = endpoint.endsWith("/")
+                ? new String[]{endpoint.substring(0, endpoint.length() - 1), endpoint, endpoint + "**"}
+                : new String[]{endpoint, endpoint + "/", endpoint + "/**"};
+        registry
+                // 2
+                .addResourceHandler(endpointPatterns)
+                .addResourceLocations(location.endsWith("/") ? location : location + "/")
+                .resourceChain(false)
+                // 3
+                .addResolver(new PathResourceResolver() {
+                    @Override
+                    public Resource resolveResource(HttpServletRequest request, String requestPath, List<? extends Resource> locations, ResourceResolverChain chain) {
+                        Resource resource = super.resolveResource(request, requestPath, locations, chain);
+                        if (nonNull(resource)) {
+                            return resource;
+                        }
+                        return super.resolveResource(request, "/index.html", locations, chain);
+                    }
+                });
     }
 }*/
