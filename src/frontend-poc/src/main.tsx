f743bc5f2fa06705013a1ffe5face9225d270734
AllasKevin <kevin_ruonala@hotmail.com>
2025-04-09 13:21:13 +0200

    Fixed protectedRoute so that it now sets isAuthenticated in localStorage, aslo change the logged-in path to /app and the loggedIn component do Dashboard.

--- src/frontend-poc/src/main.tsx
+++ src/frontend-poc/src/main.tsx
@@ -3,16 +3,14 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./components/auth/AuthContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AuthProvider>
       <BrowserRouter>
-        <Routes>
-          <Route path="/*" element={<App />} />
-        </Routes>
+        <App />
       </BrowserRouter>
     </AuthProvider>
   </StrictMode>
