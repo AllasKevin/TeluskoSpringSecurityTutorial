54738cde4cc6f5ece923c55f0c0e0ebcc00cc587
AllasKevin <kevin_ruonala@hotmail.com>
2025-04-15 17:37:25 +0200

    Added basic WebRTC functionality that does not require authentication.

--- src/frontend-poc/src/App.tsx
+++ src/frontend-poc/src/App.tsx
@@ -1,23 +1,110 @@
+import { useState, useRef } from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
-import LoginForm from "./components/LoginForm";
+//import LoginForm from "./components/LoginForm";
 import ProtectedRoute from "./components/auth/ProtectedRoute";
-import Dashboard from "./components/Dashboard";
+import Dashboard, { CallData } from "./components/Dashboard";
 import Header from "./components/Header";
+import CallerVideo from "./webrtc/Components/CallerVideo";
+import AnswerVideo from "./webrtc/Components/AnswerVideo";
+
+export interface CallStatus {
+  haveMedia: boolean;
+  videoEnabled: boolean;
+  audioEnabled: boolean;
+  callInitiated: boolean;
+  current?: string;
+  answer?: RTCSessionDescriptionInit;
+  myRole?: "offer" | "answer";
+}
 
 function App() {
+  const [callStatus, updateCallStatus] = useState<CallStatus>();
+  const [localStream, setLocalStream] = useState<MediaStream>();
+  const [remoteStream, setRemoteStream] = useState<MediaStream>();
+  const [peerConnection, setPeerConnection] = useState<RTCPeerConnection>();
+  const [userName, setUserName] = useState<string | null>(null);
+  const [offerData, setOfferData] = useState<CallData>();
+  const remoteFeedEl = useRef<HTMLVideoElement>(null); //this is a React ref to a dom element, so we can interact with it the React way
+  const localFeedEl = useRef<HTMLVideoElement>(null); //this is a React ref to a dom element, so we can interact with it the React way
   return (
     <div>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/loginpage" element={<LoginForm />} />
+        <Route
+          path="/loginpage"
+          element={
+            <Dashboard
+              callStatus={callStatus}
+              updateCallStatus={updateCallStatus}
+              //                localStream={localStream}
+              setLocalStream={setLocalStream}
+              remoteStream={remoteStream}
+              setRemoteStream={setRemoteStream}
+              peerConnection={peerConnection}
+              setPeerConnection={setPeerConnection}
+              userName={userName}
+              setUserName={setUserName}
+              //                offerData={offerData}
+              setOfferData={setOfferData}
+              remoteFeedEl={remoteFeedEl}
+              localFeedEl={localFeedEl}
+            />
+          }
+        />
         <Route
           path="/app"
           element={
             <ProtectedRoute>
-              <Dashboard />
+              <Dashboard
+                callStatus={callStatus}
+                updateCallStatus={updateCallStatus}
+                //                localStream={localStream}
+                setLocalStream={setLocalStream}
+                remoteStream={remoteStream}
+                setRemoteStream={setRemoteStream}
+                peerConnection={peerConnection}
+                setPeerConnection={setPeerConnection}
+                userName={userName}
+                setUserName={setUserName}
+                //                offerData={offerData}
+                setOfferData={setOfferData}
+                remoteFeedEl={remoteFeedEl}
+                localFeedEl={localFeedEl}
+              />
             </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/offer"
+          element={
+            <CallerVideo
+              callStatus={callStatus}
+              updateCallStatus={updateCallStatus}
+              localStream={localStream}
+              remoteStream={remoteStream}
+              peerConnection={peerConnection}
+              userName={userName}
+              remoteFeedEl={remoteFeedEl}
+              localFeedEl={localFeedEl}
+            />
+          }
+        />
+        <Route
+          path="/answer"
+          element={
+            <AnswerVideo
+              callStatus={callStatus}
+              updateCallStatus={updateCallStatus}
+              localStream={localStream}
+              remoteStream={remoteStream}
+              peerConnection={peerConnection}
+              userName={userName}
+              offerData={offerData}
+              remoteFeedEl={remoteFeedEl}
+              localFeedEl={localFeedEl}
+            />
           }
         />
       </Routes>
--- src/frontend-poc/src/components/Dashboard.tsx
+++ src/frontend-poc/src/components/Dashboard.tsx
@@ -1,10 +1,62 @@
-import { useState } from "react";
+import { RefObject, useEffect, useState } from "react";
 import apiClient from "../services/api-client";
+import socketConnection from "../webrtc/webrtcUtilities/socketConnection";
+import createPeerConnection from "../webrtc/webrtcUtilities/createPeerConn";
+import { useNavigate } from "react-router-dom";
+import { CallStatus } from "../App";
+import prepForCall from "../webrtc/webrtcUtilities/prepForCall";
+import clientSocketListeners from "../webrtc/webrtcUtilities/clientSocketListeners";
 
 interface Color {
   color: string;
 }
-const Dashboard = () => {
+export interface CallData {
+  offererUserName?: string | null;
+  offer: RTCSessionDescriptionInit;
+  offerIceCandidates: RTCIceCandidateInit[];
+  answer: RTCSessionDescriptionInit;
+  answererUserName?: string | null;
+  answererIceCandidates: RTCIceCandidateInit[];
+}
+interface DashboardProps {
+  callStatus: CallStatus | undefined;
+  updateCallStatus: React.Dispatch<
+    React.SetStateAction<CallStatus | undefined>
+  >;
+  //  localStream: MediaStream | undefined;
+  setLocalStream: React.Dispatch<React.SetStateAction<MediaStream | undefined>>;
+  remoteStream: MediaStream | undefined;
+  setRemoteStream: React.Dispatch<
+    React.SetStateAction<MediaStream | undefined>
+  >;
+  peerConnection: RTCPeerConnection | undefined;
+  setPeerConnection: React.Dispatch<
+    React.SetStateAction<RTCPeerConnection | undefined>
+  >;
+  userName: string | null;
+  setUserName: React.Dispatch<React.SetStateAction<string | null>>;
+  //  offerData: any;
+  setOfferData: React.Dispatch<React.SetStateAction<any>>;
+  remoteFeedEl: RefObject<HTMLVideoElement | null>;
+  localFeedEl: RefObject<HTMLVideoElement | null>;
+}
+
+const Dashboard = ({
+  callStatus,
+  updateCallStatus,
+  //  localStream,
+  setLocalStream,
+  remoteStream,
+  setRemoteStream,
+  peerConnection,
+  setPeerConnection,
+  userName,
+  setUserName,
+  //  offerData,
+  setOfferData,
+  remoteFeedEl,
+  localFeedEl,
+}: DashboardProps) => {
   const getStudents = () => {
     apiClient
       .get("/students", {
@@ -43,6 +95,137 @@ const Dashboard = () => {
         console.error("Error fetching changecolor:", err);
       });
   };
+
+  console.log("Dashboard component rendered");
+
+  const [typeOfCall, setTypeOfCall] = useState("");
+  const [joined, setJoined] = useState(false);
+  const [availableCalls, setAvailableCalls] = useState([]);
+  const navigate = useNavigate();
+
+  //called on "Call" or "Answer"
+  const initCall = async (typeOfCall: string) => {
+    // set localStream and GUM
+    console.log("initCall called");
+    console.log(callStatus);
+    await prepForCall({ callStatus, updateCallStatus, setLocalStream });
+    // console.log("gum access granted!")
+    setTypeOfCall(typeOfCall); //offer or answer
+  };
+
+  //Test backend connection
+  // useEffect(()=>{
+  //     const test = async()=>{
+  //         const socket = socketConnection("test")
+  //     }
+  //     test()
+  // },[])
+
+  //Nothing happens until the user clicks join
+  //(Helps with React double render)
+  useEffect(() => {
+    console.log("joined changed: " + joined);
+    if (joined) {
+      const userName = prompt("Enter username");
+      setUserName(userName);
+      const setCalls = (data: []) => {
+        setAvailableCalls(data);
+        console.log("Available calls: " + data);
+      };
+      const socket = socketConnection(userName);
+      socket.on("availableOffers", setCalls);
+      socket.on("newOfferAwaiting", setCalls);
+    }
+  }, [joined]);
+
+  //We have media via GUM. setup the peerConnection w/listeners
+  useEffect(() => {
+    if (callStatus && userName && callStatus.haveMedia && !peerConnection) {
+      // prepForCall has finished running and updated callStatus
+      const result = createPeerConnection(userName, typeOfCall);
+      if (result) {
+        setPeerConnection(result.peerConnection);
+        setRemoteStream(result.remoteStream);
+      }
+    }
+  }, [callStatus?.haveMedia]);
+
+  //We know which type of client this is and have PC.
+  //Add socketlisteners
+  useEffect(() => {
+    if (typeOfCall && peerConnection) {
+      const socket = socketConnection(userName);
+      clientSocketListeners(
+        socket,
+        typeOfCall,
+        callStatus,
+        updateCallStatus,
+        peerConnection,
+        remoteFeedEl,
+        localFeedEl
+      );
+    }
+  }, [typeOfCall, peerConnection]);
+
+  //once remoteStream AND pc are ready, navigate
+  useEffect(() => {
+    if (remoteStream && peerConnection) {
+      navigate(`/${typeOfCall}?token=${Math.random()}`);
+    }
+  }, [remoteStream, peerConnection]);
+
+  const call = async () => {
+    //call related stuff goes here
+    initCall("offer");
+  };
+
+  const answer = (callData: CallData) => {
+    //answer related stuff goes here
+    initCall("answer");
+    setOfferData(callData);
+  };
+
+  if (!joined) {
+    return (
+      <div className="container d-flex align-items-center justify-content-center min-vh-100">
+        <button
+          onClick={() => setJoined(true)}
+          className="btn btn-primary btn-lg"
+        >
+          Join
+        </button>
+      </div>
+    );
+  }
+
+  return (
+    <div className="container">
+      <div className="row">
+        <h1>{userName}</h1>
+        <div className="col-6">
+          <h2>Make a call</h2>
+          <button onClick={call} className="btn btn-success btn-lg hang-up">
+            Start Call
+          </button>
+        </div>
+        <div className="col-6">
+          <h2>Available Calls</h2>
+          {availableCalls.map((callData: CallData, i) => (
+            <div className="col mb-2" key={i}>
+              <button
+                onClick={() => {
+                  answer(callData);
+                }}
+                className="btn btn-lg btn-warning hang-up"
+              >
+                Answer Call From {callData.offererUserName}
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 
   return (
     <div
