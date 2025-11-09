6f7c8907c7ace39575be3b0151d529fb67c4f3fe
AllasKevin <kevin_ruonala@hotmail.com>
2025-11-09 13:26:00 +0100

    Lagt till ny const practice som är shared mellan frontend och signalingServer och nu är det implementerat så att den används i frontend i practicesPage istället för den lokala variablen practices. Jag har även flyttat profilbilderna från /assets till /public

---
+++ src/shared/practices/practices.js
@@ -0,0 +1,58 @@
+export const practices = [
+    {
+      title: "Asking Practice",
+      name: "askingpractice",
+      description:
+        "Practice what it feels like to play around with asking for what ever comes to mind, or go beyond and connect with and express your deepest wishes.",
+      imageUrl: "/profilepictures/lore.jpg",
+      videoUrl: "/instructionvideos/stock_woman_talking.mp4",
+    },
+    {
+      title: "Noticing Game",
+      name: "noticinggame",
+      description:
+        "Explore what experiences are revealed inside yourself in the present moment and relate those authentically to your co-explorer.",
+      imageUrl: "/profilepictures/nadine.jpg",
+      videoUrl: "/instructionvideos/noticing_game_fixed.mp4"
+    },
+    {
+      title: "Assert & Protect",
+      name: "assertandprotect",
+      description:
+        "Practice how to assert and protect your boundaries in a safe space.",
+      imageUrl: "/profilepictures/gabor-mate.jpg",
+      videoUrl: "https://media.istockphoto.com/id/1370809321/sv/video/smiling-young-female-tutor-talking-to-web-camera-recording-vlog-online-video-conference-video.mp4?s=mp4-640x640-is&k=20&c=LEFPdcDbab1YLOLXSqo9JYJsdGRbI-SaLygESHD9rkE=",
+    },
+    {
+      title: "Gratitude Practice",
+      name: "gratitudepractice",
+      description:
+        "Increase your awareness of the positive aspects of your life and cultivate a sense of gratitude.",
+      imageUrl: "/profilepictures/tony-robbins.webp",
+      videoUrl: "/instructionvideos/noticing_game_fixed.mp4"
+    },
+    {
+      title: "Kirtan",
+      name: "kirtan",
+      description:
+        "Engage in guided call-and-response chanting of sacred names to harmonize breath, voice, and intention.",
+      imageUrl: "/profilepictures/dileva.jpg",
+      videoUrl: "/instructionvideos/noticing_game_fixed.mp4"
+    },
+    {
+      title: "Non-Contact Improv",
+      name: "noncontactimprov",
+      description:
+        "Reflect each other’s bodily directional impulses by mirroring small initiating movements and amplifying dynamic flow.",
+      imageUrl: "/profilepictures/Profilbild.jpg",
+      videoUrl: "/instructionvideos/noticing_game_fixed.mp4"
+    },
+    {
+      title: "Echo of True Self",
+      name: "echooftrueself",
+      description:
+        "Speak your truth and experience the power of being heard and affirmed by another.",
+      imageUrl: "/profilepictures/teal-swan.webp",
+      videoUrl: "/instructionvideos/noticing_game_fixed.mp4"
+    },
+];
