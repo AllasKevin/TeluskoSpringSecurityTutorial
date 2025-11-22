bffdc538d14a516602fff54cb5622b0ea2afff28
AllasKevin <kevin_ruonala@hotmail.com>
2025-11-22 19:30:25 +0100

    Increased length of asking practice

--- src/frontend-poc/src/practices/practiceConfigs.ts
+++ src/frontend-poc/src/practices/practiceConfigs.ts
@@ -1,7 +1,7 @@
 export const practiceConfigs = {
   askingpractice: {
-    duration: 202000,
-    endingDuration: 20000, // 20 seconds countdown
+    duration: 262000,
+    endingDuration: 60000, // 60 seconds countdown
     audioCues: [
       { time: 2_000, src: "/audio/asking_practice_instructions_1.mp3" },
       { time: 62_000, src: "/audio/asking_practice_instructions_2.mp3" },
